7d44301349b7cde93be512e569999d16d9c114e8
Gwanghyeon-k <hsys06207@gmail.com>
2024-12-28 03:26:19 +0900

    study: 2장 문제 2번 정답

--- src/class3/problem2/Account.java
+++ src/class3/problem2/Account.java
@@ -1,21 +1,26 @@
 package class3.problem2;
 
 public class Account {
-    int balance = 0;
+
+    //인스턴스 변수는 생성자나 초기화 블록에서 명시적으로 초기화하지 않아도 자동으로 0으로 설정된다.
+    //지역변수는 초기화해야됨!
+    int balance;
+
+    // deposit과 withdraw는 잔액이라는 상태를 변경하는 것이 목적이지 변경된 값을 직접 반환하는 것이 주목적이 아니기 때문에
+    // 객체지향적인 관점에 어긋난다. 그래서 int로 반환값을 주는 것보다 void를 사용하는 것이 낫다
 
     //입금 메서드
-    int deposit(int amount){
-        return balance += amount;
+    void deposit(int amount){
+        balance += amount;
     }
 
     //출금 메서드
-    int withdraw(int amount) {
+    void withdraw(int amount) {
         if(balance >= amount){
             balance -= amount;
         }
         else{
             System.out.println("잔액 부족");
         }
-        return balance;
     }
 }
--- src/class3/problem2/AccountMain.java
+++ src/class3/problem2/AccountMain.java
@@ -4,14 +4,11 @@ public class AccountMain {
     public static void main(String[] args) {
         Account account = new Account();
 
-        int amount = 10000;
-        account.balance = account.deposit(amount);
-
-        int amount2 = 9000;
-        account.withdraw(amount2);
-
-        int amount3 = 2000;
-        account.withdraw(amount3);
+        //int로 주지 말고 그냥 넣어버려
+        //애초에 함수를 호출하면 balance값의 상태가 변경되므로 굳이 다시 = 으로 넣어주지 않아도 됨
+        account.deposit(10000);
+        account.withdraw(9000);
+        account.withdraw(2000);
 
         System.out.println("잔고: " + account.balance);
 
