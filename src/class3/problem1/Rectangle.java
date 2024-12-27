c154f5d0a3305dbe143da2e983b54d7b1b897a1f
Gwanghyeon-k <hsys06207@gmail.com>
2024-12-28 02:55:06 +0900

    study: 2장 문제 1 정답

--- src/class3/problem1/Rectangle.java
+++ src/class3/problem1/Rectangle.java
@@ -3,16 +3,19 @@ package class3.problem1;
 public class Rectangle {
     int width;
     int height;
-    int area;
-    int perimeter;
-    boolean square;
-    static int calculateArea(int width, int height){
+    // 사각형 객체에 이런 인자들은 필요가 없음
+//    int area;
+//    int perimeter;
+//    boolean square;
+    int calculateArea(){
         return width*height;
     }
-    static int calculatePerimeter(int width, int height){
+    int calculatePerimeter(){
         return 2*(width+height);
     }
-    static boolean isSquare(int width, int height){
+    boolean isSquare(){
         return width == height;
     }
+    // static을 사용하지 않게 해서 객체를 호출하지 않아도 사용할 수 있게끔 수정하고
+    // return width -> 결국 return this.width에서 this가 생략된 형태임
 }
--- src/class3/problem1/RectangleOopMain.java
+++ src/class3/problem1/RectangleOopMain.java
@@ -6,13 +6,15 @@ public class RectangleOopMain {
         rectangle.width = 5;
         rectangle.height = 8;
 
-        rectangle.area = rectangle.calculateArea(rectangle.width, rectangle.height);
-        rectangle.perimeter = rectangle.calculatePerimeter(rectangle.width, rectangle.height);
+        // 직접 인자를 넣어주지 않아도 됨
+        //인스턴스 메서드에서는 필드(멤버 변수) 자체에 접근할 수 있으므로 인자를 전달할 필요가 없음
+        int area = rectangle.calculateArea();
+        int perimeter = rectangle.calculatePerimeter();
 
-        rectangle.square = rectangle.isSquare(rectangle.width, rectangle.height);
+        boolean square = rectangle.isSquare();
 
-        System.out.println("넓이: " + rectangle.area);
-        System.out.println("둘레 길이: " + rectangle.perimeter);
-        System.out.println("정사각형 여부: " + rectangle.square);
+        System.out.println("넓이: " + area);
+        System.out.println("둘레 길이: " + perimeter);
+        System.out.println("정사각형 여부: " + square);
     }
 }
