e046d436b4779d43e33c13dee5b3558c2e2e5117
Gwanghyeon-k <hsys06207@gmail.com>
2024-12-28 02:59:04 +0900

    study: 2장 1번 주석 추가

--- src/class3/problem1/Rectangle.java
+++ src/class3/problem1/Rectangle.java
@@ -18,4 +18,5 @@ public class Rectangle {
     }
     // static을 사용하지 않게 해서 객체를 호출하지 않아도 사용할 수 있게끔 수정하고
     // return width -> 결국 return this.width에서 this가 생략된 형태임
+    // width와 height가 객체 안에 속하는 인자이기 때문에 따로 전달해주지 않아도 됨 만약 다른 area 같은 것을 필요로 한다면 전달해줘야됨
 }
