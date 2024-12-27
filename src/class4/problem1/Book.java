c59ec8014a8c78c0708873a4a84fff468730c88d
Gwanghyeon-k <hsys06207@gmail.com>
2024-12-28 06:35:02 +0900

    [study] 4장 생성자 문제 1 내 풀이

---
+++ src/class4/problem1/Book.java
@@ -0,0 +1,30 @@
+package class4.problem1;
+
+public class Book {
+    String title;
+    String author;
+    int page;
+
+    Book(){
+        this.title = " ";
+        this.author = " ";
+        this.page = 0;
+    }
+
+    Book(String title, String author){
+        this.title = title;
+        this.author = author;
+        this.page = 0;
+    }
+
+    Book(String title, String author, int page){
+        this.title = title;
+        this.author = author;
+        this.page = page;
+    }
+
+    void displayInfo(){
+        System.out.println("제목: " + title + ", 저자: " + author + ""
+                + ", 페이지: " + page);
+    }
+}
